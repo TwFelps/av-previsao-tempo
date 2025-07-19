1569b67753fd37bd2465d7854d96cb262bd466f1
TwFelps <lifipelp@gmail.com>
2025-07-19 18:09:21 -0300

    Estrutura inicial do projeto: organização de pastas e telas

---
+++ source/screens/HomeScreen.tsx
@@ -0,0 +1,24 @@
+// source/screens/Home.jsx
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Previsão do Tempo</Text>
+      {/* Aqui você vai colocar o campo de texto, botão e os dados do clima */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
